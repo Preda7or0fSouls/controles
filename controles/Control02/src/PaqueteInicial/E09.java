bece377fba842c05eef93151c703d8a7e0c9a1c2
CARLOS <carlos.soler.roig1994@gmail.com>
2021-11-08 09:22:06 +0100

    enunciados copiados 7,9,10

--- controles/Control02/src/PaqueteInicial/E09.java
+++ controles/Control02/src/PaqueteInicial/E09.java
@@ -1,9 +1,15 @@
 package PaqueteInicial;
 
 public class E09 {
+/*
 
+Escriba un programa que pregunte al usuario las notas de los dos primeros certamen
+ y la nota de laboratorio, y muestre la nota que necesita el alumno para aprobar 
+ el ramo con nota final 60.
+
+*/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
 	}
 
--- controles/Control02/src/PaqueteInicial/E10.java
+++ controles/Control02/src/PaqueteInicial/E10.java
@@ -1,9 +1,13 @@
 package PaqueteInicial;
 
 public class E10 {
-
+/*
+Escriba un programa que reciba como entrada la temperatura original del huevo 
+y muestre como salida el tiempo en segundos que le toma alcanzar
+ la temperatura máxima para prepararlo a la copa.
+*/ 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
