01cd7580bf170bf32a8dbc9fa8ba864d7857701a
CARLOS <carlos.soler.roig1994@gmail.com>
2021-11-10 08:25:31 +0100

    ejer 7 a medias

--- controles/Control02/src/PaqueteInicial/E07.java
+++ controles/Control02/src/PaqueteInicial/E07.java
@@ -4,7 +4,8 @@ public class E07 {
 	/*Escriba un programa que pregunte al usuario la hora actual t del reloj y un número entero de horas h, 
 	que indique qué hora marcará el reloj dentro de h horas:*/
 	public static void main(String[] args) {
-		
+		int t, incremento,HoraDef;
+
 
 	}
 
